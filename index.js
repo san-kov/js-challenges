580489b5f30b099dcfb0e54761ef1a618dd2004e
Beholdert <sanoktabx@gmail.com>
2019-01-10 01:05:36 +0600

    Closures, Scope, Executional Context

---
+++ index.js
@@ -0,0 +1,102 @@
+function createFunction() {
+  return function() {
+    console.log('hello');
+  };
+}
+
+// var function1 = createFunction();
+// function1();
+
+function createFunctionPrinter(input) {
+  return function() {
+    console.log(input);
+  };
+}
+
+// var printSample = createFunctionPrinter('sample');
+// printSample();
+// var printHello = createFunctionPrinter('hello');
+// printHello();
+
+function outer() {
+  var counter = 0; // this variable is outside incrementCounter's scope
+  function incrementCounter() {
+    counter++;
+    console.log('counter', counter);
+  }
+  return incrementCounter;
+}
+
+var willCounter = outer();
+var jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+willCounter();
+willCounter();
+willCounter();
+
+jasCounter();
+willCounter();
+
+function addByX(x) {
+  return y => x + y;
+}
+
+var addByTwo = addByX(2);
+
+console.log(addByTwo(1));
+
+console.log(addByTwo(2));
+
+function once(func) {
+  let called = false;
+  let result = 0;
+
+  return function(x) {
+    if (!called) {
+      result = func(x);
+
+      called = true;
+      return result;
+    }
+
+    return result;
+  };
+}
+
+var onceFunc = once(addByTwo);
+
+console.log(onceFunc(4)); //should log 6
+console.log(onceFunc(10)); //should log 6
+console.log(onceFunc(9001)); //should log 6
+
+function after(count, func) {
+  let counter = 0;
+
+  return function() {
+    counter++;
+
+    if (counter >= count) {
+      func();
+    }
+  };
+}
+
+var called = function() {
+  console.log('hello');
+};
+var afterCalled = after(3, called);
+
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> 'hello' is printed
+
+function delay(func, wait) {
+  setTimeout(() => {
+    func();
+  }, wait);
+}
+
+delay(() => console.log('123123'), 5200);
